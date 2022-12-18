9082e37984b912e633a8d98bce6bad377ed00956
ryuwc <wom05169@gmail.com>
2022-12-18 17:29:31 +0900

    Update S2_1926.java

--- src/baekjoon/Silver/S2_1926.java
+++ src/baekjoon/Silver/S2_1926.java
@@ -1,6 +1,8 @@
 package baekjoon.Silver;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 
@@ -8,11 +10,10 @@ import java.util.*;
 public class S2_1926 {
     public static int N, M;
     public static int[][] arr;
-    public static Set<String> visit = new HashSet<>();
-    public static Deque<Integer> Q = new ArrayDeque<>();
     public static int rst, cnt;
     public static int[] di = {0, 0, 1, -1};
     public static int[] dj = {1, -1, 0, 0};
+    public static int[][] visit;
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String[] nm = bf.readLine().split(" ");
@@ -30,11 +31,10 @@ public class S2_1926 {
 
         cnt = 0;
         rst = 0;
+        visit = new int[N][M];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                // i, j를 String으로 만들기
-                String now = String.valueOf(i) + String.valueOf(j);
-                if (!visit.contains(now) && arr[i][j] == 1) {
+                if (visit[i][j] == 0 && arr[i][j] == 1) {
                     bfs(i, j);
                     cnt += 1;
                 }
@@ -43,35 +43,34 @@ public class S2_1926 {
         }
 
         System.out.println(cnt);
-        System.out.print(rst);
+        System.out.println(rst);
+
     }
     public static void bfs(int i, int j) {
+        Deque<Integer> Q = new ArrayDeque<>();
         // 큐에 넣기
         Q.add(i);
         Q.add(j);
         // 방문처리
-        visit.add(String.valueOf(i) + String.valueOf(j));
+        visit[i][j] = 1;
         // 큐가 빌때까지
 
         int tmpRst = 1;
         while (!Q.isEmpty()) {
-            int r = Q.pollFirst();
-            int c = Q.pollFirst();
+            int r = Q.poll();
+            int c = Q.poll();
             for (int k = 0; k < 4; k++) {
                 int nr = r + di[k];
                 int nc = c + dj[k];
                 if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-                String o = String.valueOf(nr) + String.valueOf(nc);
-                if (visit.contains(o)) continue;
-                if (arr[nr][nc] == 0) continue;
-                tmpRst += 1;
-                Q.add(nr);
-                Q.add(nc);
-                visit.add(o);
+                if (visit[nr][nc] == 0 && arr[nr][nc] == 1) {
+                    tmpRst += 1;
+                    Q.add(nr);
+                    Q.add(nc);
+                    visit[nr][nc] = 1;
+                }
             }
         }
-
         rst = Math.max(rst, tmpRst);
-
     }
 }
